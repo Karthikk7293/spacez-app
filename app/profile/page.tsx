3ef09d1e2ee41e9abe6aeda87c87b7c2ca0744f4
Karthikk7293 <karthikk7293@gmail.com>
2025-05-13 10:22:04 +0530

    👌 IMPROVE:

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -12,7 +12,7 @@ const ProfilePage = () => {
             <header className="border-b border-b-gray-200 px-5">
                 <div className="flex  justify-between items-center  ">
                     <div className="">
-                        <Image className="h-auto w-auto" height={50} width={100} src={'https://res.cloudinary.com/dz3tveb47/image/upload/v1741425295/logo_for_websiter_1_1_iz3hbc.png'} alt="logo" />
+                        <Image className="h-auto w-auto" height={100} width={100} src={'https://res.cloudinary.com/dz3tveb47/image/upload/v1741425295/logo_for_websiter_1_1_iz3hbc.png'} alt="logo" />
                     </div>
                     <div className="flex flex-col gap-1">
                         <div className="border w-5" />
@@ -43,7 +43,7 @@ const ProfilePage = () => {
                         <Image width={160} height={160} alt="avatar" src={"/avatar.webp"} className="rounded-full" />
                         {/* <PencilSquareIcon className="w-6 h-6 absolute" /> */}
 
-                        <span className="absolute right-3 bottom-2 border rounded p-1 border-gray-200 bg-white">
+                        <span className="absolute right-3 bottom-2 border rounded p-1 border-gray-100 bg-white text-gray-400">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
                                 <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
                                 <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
@@ -69,9 +69,9 @@ const ProfilePage = () => {
                 </div>
                 <hr className="text-gray-200" />
                 <div className="pt-5">
-                    <InputBox name={"email"} type={"text"} placeHolder={"Legal Name"} required={true} readOnly={false} isBlur={false} state="" />
-                    <InputBox name={"mobile"} type={"text"} placeHolder={"Phone Number"} required={true} readOnly={false} isBlur={false} state="" />
-                    <InputBox name={"email"} type={"email"} placeHolder={"Email"} required={true} readOnly={false} isBlur={false} state="" />
+                    <InputBox name={"email"} type={"text"} placeHolder={"Legal Name"} required={true} readOnly={false} isBlur={false} state="" showEdit={true} />
+                    <InputBox name={"mobile"} type={"text"} placeHolder={"Phone Number"} required={true} readOnly={false} isBlur={false} state="" showEdit={false} />
+                    <InputBox name={"email"} type={"email"} placeHolder={"Email"} required={true} readOnly={false} isBlur={false} state="" showEdit={true} />
                 </div>
             </div>
             <footer className="border fixed bottom-0 h-16 w-full">
--- components/ui/InputBox.tsx
+++ components/ui/InputBox.tsx
@@ -7,15 +7,17 @@ interface InputBoxProps {
     required: boolean,
     readOnly: boolean,
     state: string,
-    isBlur: boolean
+    isBlur: boolean,
+    showEdit: boolean
 }
 
-const InputBox = ({ type, name, placeHolder, required, readOnly, state }: InputBoxProps) => {
+const InputBox = ({ type, name, placeHolder, required, readOnly, state, showEdit }: InputBoxProps) => {
 
     // const [blur, setBlur]: [boolean, Dispatch<SetStateAction<boolean>>] = useState(false)
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         e.preventDefault()
+
     }
 
     // const handleBlur = (status: boolean) => {
@@ -23,9 +25,9 @@ const InputBox = ({ type, name, placeHolder, required, readOnly, state }: InputB
     // }
 
     return (
-        <div>
+        <div className="flex border items-center border-b-2 my-2">
             <input
-                className="border border-b-2 w-full p-3 my-2"
+                className=" w-full p-3 !border-transparent focus:outline-none "
                 type={type}
                 name={name}
                 placeholder={placeHolder}
@@ -35,6 +37,12 @@ const InputBox = ({ type, name, placeHolder, required, readOnly, state }: InputB
                 onChange={((e) => handleChange(e))}
 
             />
+            {showEdit && <span className=" px-2 ">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5 text-gray-300">
+                    <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
+                    <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
+                </svg>
+            </span>}
         </div>
     )
 }
